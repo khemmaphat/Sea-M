02c6f91a703d57209b429b9be267775d150c28b2
khemmaphat <khemmaphat.work@gmail.com>
2022-10-21 20:26:20 +0700

    add column in login.js with tailwind

--- sea-m/src/auth/Login.js
+++ sea-m/src/auth/Login.js
@@ -4,16 +4,20 @@ import { loginEndpoint } from '../contents/spotify'
 
 export default function login() {
   return (
-    <div className='font-face-ds'>
-      <div className=''>
-        <h1>You are the middle in the sea</h1>
+    <div class="font-face-ds grid grid-cols-3 gap-4">
+      <div></div>
+      <div class="col-start-2 col-span-4">
+          <div>
+          <h1>You are the middle in the sea</h1>
+          </div>
+            <img class='scale-75' src="https://cdn.discordapp.com/attachments/1032286364315680818/1032286398159523870/unknown.png"/>
+          <div>
+            <h3>Login to sail to nearby island</h3>
+            <p>This website is part of the CPE327 Software Engineering Project.</p>
+          </div>
+        <a href={loginEndpoint}><div className='login-btn'>Log-in</div></a>
       </div>
-      <img class='' src="https://cdn.discordapp.com/attachments/1032286364315680818/1032286398159523870/unknown.png"/>
-      <div>
-        <h3>Login to sail to nearby island</h3>
-        <p>This website is part of the CPE327 Software Engineering Project.</p>
-      </div>
-      <a href={loginEndpoint}><div className='login-btn'>Log-in</div></a>
+      <div></div>
     </div>
   )
 }
