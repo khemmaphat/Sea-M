eb6ccbca292f344cc4eb7a74e1bc4474a046006d
khemmaphat <khemmaphat.work@gmail.com>
2022-10-26 20:53:31 +0700

    Edit Homeapge.js

--- sea-m/src/contents/Homepage.js
+++ sea-m/src/contents/Homepage.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from '../img/middle-sea.png'
 
 export default function Homepage() {
   return (
     <div>
-       <p>Hello World</p> 
+       <div >
+        <img src={logo}></img>
+          <h1>Sea-Music</h1>
+       </div>
     </div>
   )
 }
