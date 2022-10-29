44d6a43d9517aeb56129a2a6f2e3ffd974571aad
khemmaphat <khemmaphat.work@gmail.com>
2022-10-29 17:09:14 +0700

    update Hpmepage.js

--- sea-m/src/contents/Homepage.js
+++ sea-m/src/contents/Homepage.js
@@ -4,11 +4,11 @@ import { IconContext } from 'react-icons';
 import { AiFillPlayCircle } from 'react-icons/ai';
 import { useNavigate } from 'react-router';
 
-import logo from '../img/Logo.jpg'
+
 import logoName from '../img/Logo-name.png'
-import MusicButton from '../img/spotify.png'
-import AboutUs from '../img/About-us.png'
-import Github from '../img/github.png'
+import MusicButton from '../img/Music-Button.png'
+import AboutUs from '../img/About_Us_Button.png'
+import Github from '../img/GitHub-Button.png'
 
 
 export default function Homepage() {
@@ -29,15 +29,14 @@ export default function Homepage() {
     <div className="font-face-ds">
       <div className="fixed left-5 top-5">
         <div className="flex space-x-4">
-          <img className="flex-none w-32 rounded-2xl" src={logo}/>
-          <img className="flex-initial w-64" src={logoName}/>
-          <img className="flex-initial w-12" src={MusicButton}/>
+          <img className="flex-none w-72 h-36" src={logoName}/>
+          <img className="flex-auto h-[5.5rem] mt-7" src={MusicButton}/>
         </div>
       </div>
       <div className="fixed left-5 bottom-5">
         <div className="flex space-x-4">
-          <img className="flex-none w-32" src={AboutUs}/>
-          <img className="flex-initial w-12 rounded-full" src={Github}/>
+          <img className="flex-none w-40 h-20" src={AboutUs}/>
+          <img className="flex-auto w-12 h-12 mt-6" src={Github}/>
         </div>
       </div>
 
