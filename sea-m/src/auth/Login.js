1a8e1088e28da8cab76eb80d44ed33e743965d1c
BaNafee <natad2544@gmail.com>
2022-10-30 18:19:06 +0700

    add role

--- sea-m/src/auth/Login.js
+++ sea-m/src/auth/Login.js
@@ -6,10 +6,10 @@ export default function login() {
   return (
     <div class="font-face-ds grid grid-cols-4">
       <div class="col-start-2 col-end-4">
-          <div class="text-4xl text-center">
+          <div class="text-4xl text-center" >
           <h1>You are the middle in the sea</h1>
-          </div>
-            <img class="scale-75 mx-auto" src="https://cdn.discordapp.com/attachments/1032286364315680818/1032286398159523870/unknown.png"/>
+          </div >
+            <img class="scale-75 mx-auto"  alt="Boat" src="https://cdn.discordapp.com/attachments/1032286364315680818/1032286398159523870/unknown.png"/>
           <div class="text-2xl">
             <h3>Login to sail to nearby island</h3>
           </div>
@@ -18,7 +18,7 @@ export default function login() {
           </div>
            
           
-        <a href={loginEndpoint}><div className="login-btn" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-4 mt-2 rounded inline-flex items-center text-center">Log-in</div></a>
+        <a href={loginEndpoint}  role="button" ><div className="login-btn" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-4 mt-2 rounded inline-flex items-center text-center">Log-in</div></a>
       </div>
     </div>
   )
