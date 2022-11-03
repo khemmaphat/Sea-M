02915cb9968d41717fce8a031e6c4ad23788047e
Aquintise <tayuth34959@gmail.com>
2022-11-04 02:59:42 +0700

    finish playlist spotify

--- sea-m/src/components/AudioPlayer.js
+++ sea-m/src/components/AudioPlayer.js
@@ -75,11 +75,26 @@ const AudioPlayer = () => {
         return (<div dangerouslySetInnerHTML={ {__html:  props.iframe?props.iframe:""}} />);
     }
 
-    const iframe = '<iframe data-v-4061fa9a="" id="spotify" src="https://open.spotify.com/embed/playlist/7Bg9W2mPUH4dkwhddHDvFK?utm_source=generator" allowfullscreen="allowfullscreen" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" style="border-radius: 12px;" width="100%" height="80" frameborder="0"></iframe>';
+    let iframe = '<iframe data-v-4061fa9a="" id="spotify" src="https://open.spotify.com/embed/playlist/7Bg9W2mPUH4dkwhddHDvFK?utm_source=generator" allowfullscreen="allowfullscreen" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" style="border-radius: 12px;" width="100%" height="80" frameborder="0"></iframe>';
+
+    function changePlaylist(e) {
+        const id = e.currentTarget.id
+        console.log(id);
+        if (id == "1") {
+            document.getElementById("spotify").src = "https://open.spotify.com/embed/playlist/2DQu4yw3Bx58eNEZYIevDl?utm_source=generator";
+        } else if (id == "2") {
+            document.getElementById("spotify").src = "https://open.spotify.com/embed/playlist/4tJ5q6xFg4dACbauIkjTCJ?utm_source=generator";
+        } else {
+            document.getElementById("spotify").src = "https://open.spotify.com/embed/playlist/7Bg9W2mPUH4dkwhddHDvFK?utm_source=generator";
+        }
+    };
 
     return (
         <div className="flex items-center justify-center">
             <Iframe iframe={iframe} />
+            <p id="1" data-v-4061fa9a="" className="text-sm mx-3 text-gray-500 hover:cursor-pointer" onClick={(e) => changePlaylist(e)}>Chill beat</p>
+            <p id="2" data-v-4061fa9a="" className="text-sm mx-3 text-gray-500 hover:cursor-pointer" onClick={(e) => changePlaylist(e)}>Dim light with lonely vibe</p>
+            <p id="3" data-v-4061fa9a="" className="text-sm mx-3 text-gray-500 hover:cursor-pointer" onClick={(e) => changePlaylist(e)}>Sentimental R&amp;B/hiphop</p>
             <audio ref={audioPlayer} preload="metadata">
                 <source src={firstSong}></source>
                 <source src={secondSong}></source>
