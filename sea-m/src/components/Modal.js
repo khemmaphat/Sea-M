12cb05ea9b76fe9fe66a98dd46c17fca377453cb
WaveWaveWaveWaveWave <wave.wave0711@gmail.com>
2022-11-27 23:46:28 +0700

    Close Button

--- sea-m/src/components/Modal.js
+++ sea-m/src/components/Modal.js
@@ -15,7 +15,7 @@ const Modal = ({ isVisible, onClose, children }) => {
         <div id="wrapper" onClick={handleClose}>
             <div className="flex justify-center m-16"> 
                 <div className="flex flex-col bg-amber-50 w-96">
-                    <button onClick={() => onClose()}><img className="hover:scale-110 w-4 h-4 ml-auto mr-2 mt-2" src={Close}/></button>
+                    <button aria-label="Close Button Please press Shift C" onClick={() => onClose()}><img className="hover:scale-110 w-4 h-4 ml-auto mr-2 mt-2" src={Close}/></button>
                     {children}
                 </div>
             </div>
